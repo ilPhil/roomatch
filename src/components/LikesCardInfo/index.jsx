239dd1cd7219f71d153d6e3a595ae139f5c19afe
kastellai <ilenia.castellana@gail.com>
2022-02-20 17:02:26 +0100

    feat: define html code block according to entity (user or room) rename prop to make it generic

--- src/components/LikesCardInfo/index.jsx
+++ src/components/LikesCardInfo/index.jsx
@@ -4,35 +4,180 @@ import { FiHeart } from "react-icons/fi";
 import { likeDislike, peoplelikeDislike } from "../../store/actions";
 import { useSelector, useDispatch } from "react-redux";
 
-const LikesCardInfo = ({ user, showInfo, setShowInfo, isRoom }) => {
+const LikesCardInfo = ({ data, showInfo, setShowInfo, isRoom }) => {
   const dispatch = useDispatch();
   const loading = useSelector((store) => store.loading);
   const loggedUser = useSelector((store) => store.user);
 
-  const dataDetails = isRoom ? user.friendlyWith : user.iam;
-  const data = user;
-  console.log("data: ", data)
+  const currentItemDetails = isRoom ? data.friendlyWith : data.iam;
+  const currentItem = data;
+  console.log("data: ", currentItem)
 
   const likeFunc = () => {
     dispatch(isRoom
       ? likeDislike(
         {
           userId: loggedUser._id,
-          ilike: data.roomId,
+          ilike: currentItem.roomId,
         },
-        data.roomId,
+        currentItem.roomId,
         "addlike"
       )
       : peoplelikeDislike(
         {
           roomId: loggedUser.roomId.roomId,
-          roomilike: data.id,
+          roomilike: currentItem.id,
         },
-        data.id,
+        currentItem.id,
         "addlike"
       )
     );
   };
+
+  const genericDetails = () => (
+    <fieldset className={styles.fieldset}>
+      <legend>Friendly for</legend>
+      <section>
+        <div className={styles.flexColumn}>
+          <div>
+            <label className={styles.labelContainer} htmlFor="lgbtq">
+              LGBTQ+
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                id="lgbtq"
+                checked={currentItemDetails.lgbtq === 1 ? true : false}
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+          <div>
+            <label className={styles.labelContainer} htmlFor="pet_owner">
+              Pet owner
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                checked={currentItemDetails.pet_owner === 1 ? true : false}
+                id="pet_owner"
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+          <div>
+            <label
+              className={styles.labelContainer}
+              htmlFor="multicultural"
+            >
+              Multicultural
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                id="multicultural"
+                checked={currentItemDetails.multicultural === 1 ? true : false}
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+        </div>
+        <div className={styles.flexColumn}>
+          <div>
+            <label className={styles.labelContainer} htmlFor="veg">
+              Veg
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                id="veg"
+                checked={currentItemDetails.veg === 1 ? true : false}
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+          <div>
+            <label className={styles.labelContainer} htmlFor="smooker">
+              Smoker
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                id="smooker"
+                checked={currentItemDetails.smooker === 1 ? true : false}
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+
+          <div>
+            <label className={styles.labelContainer} htmlFor="party">
+              Party lover
+              <input
+                readOnly
+                type="checkbox"
+                name="action"
+                id="party"
+                checked={currentItemDetails.party_lover === 1 ? true : false}
+              />
+              <span className={styles.mark}></span>
+            </label>
+          </div>
+        </div>
+      </section>
+    </fieldset>
+  );
+
+  const roomDetails = () => (
+    <>
+      <div className={styles.flexHeaderInfo}>
+        <div className={styles.headerInfo}>
+          <h3>
+            {data.roomType} room
+          </h3>
+          <div className={styles.subHeader}>
+            <p>in {data.roomAddress}</p>
+            <p>
+              {data.town} ({data.city})
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.likeBtn}>
+          <FiHeart
+            onClick={() => !loading && likeFunc()}
+            className={`${styles.outlineHeart} ${styles.icon}`}
+          />
+        </div>
+      </div>
+      {genericDetails()}
+    </>
+  );
+
+  const userDetails = () => (
+    <>
+      <div className={styles.flexHeaderInfo}>
+        <div className={styles.headerInfo}>
+          <h3>
+            {data.name} {data.surname}
+          </h3>
+          <div className={styles.subHeader}>
+            <p>
+              from {data.town} ({data.city})
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.likeBtn}>
+          <FiHeart
+            onClick={() => !loading && likeFunc()}
+            className={`${styles.outlineHeart} ${styles.icon}`}
+          />
+        </div>
+      </div>
+      {genericDetails()}
+    </>
+  );
 
   return (
     <div className={styles.background}>
@@ -40,136 +185,7 @@ const LikesCardInfo = ({ user, showInfo, setShowInfo, isRoom }) => {
         <IoIosCloseCircle onClick={() => setShowInfo(!showInfo)} />
       </div>
       <div className={styles.modalInfo}>
-        <div className={styles.flexHeaderInfo}>
-          <div className={styles.headerInfo}>
-            <h3>
-              {user.name} {user.surname}
-            </h3>
-            <div className={styles.subHeader}>
-              {!isRoom && <p>Age: {user.age}</p>}
-              <p>
-                {user.town} ({user.city})
-              </p>
-            </div>
-          </div>
-          {/* <div className={styles.likeBtn}>
-            <FiHeart className={styles.icon} />
-          </div> */}
-
-          <div className={styles.likeBtn}>
-            <FiHeart
-              onClick={() => !loading && likeFunc()}
-              className={`${styles.outlineHeart} ${styles.icon}`}
-            />
-            {/* {loggedUser.ilike.filter((like) => like.roomId === data._id).length >
-              0 ? (
-              <FaHeart
-                onClick={() => !loading && dislikeFunc()}
-                className={`${styles.fillHeart} ${styles.icon}`}
-              />
-            ) : (
-              <FiHeart
-                onClick={() => !loading && likeFunc()}
-                className={`${styles.outlineHeart} ${styles.icon}`}
-              />
-            )} */}
-          </div>
-        </div>
-
-        <fieldset className={styles.fieldset}>
-          <legend>Friendly for</legend>
-          <section>
-            <div className={styles.flexColumn}>
-              <div>
-                <label className={styles.labelContainer} htmlFor="lgbtq">
-                  LGBTQ+
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    id="lgbtq"
-                    checked={dataDetails.lgbtq === 1 ? true : false}
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-
-              <div>
-                <label className={styles.labelContainer} htmlFor="pet_owner">
-                  Pet owner
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    checked={dataDetails.pet_owner === 1 ? true : false}
-                    id="pet_owner"
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-              <div>
-                <label
-                  className={styles.labelContainer}
-                  htmlFor="multicultural"
-                >
-                  Multicultural
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    id="multicultural"
-                    checked={dataDetails.multicultural === 1 ? true : false}
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-            </div>
-
-            <div className={styles.flexColumn}>
-              <div>
-                <label className={styles.labelContainer} htmlFor="veg">
-                  Veg
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    id="veg"
-                    checked={dataDetails.veg === 1 ? true : false}
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-
-              <div>
-                <label className={styles.labelContainer} htmlFor="smooker">
-                  Smoker
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    id="smooker"
-                    checked={dataDetails.smooker === 1 ? true : false}
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-
-              <div>
-                <label className={styles.labelContainer} htmlFor="party">
-                  Party lover
-                  <input
-                    readOnly
-                    type="checkbox"
-                    name="action"
-                    id="party"
-                    checked={dataDetails.party_lover === 1 ? true : false}
-                  />
-                  <span className={styles.mark}></span>
-                </label>
-              </div>
-            </div>
-          </section>
-        </fieldset>
+        {isRoom ? roomDetails() : userDetails()}
       </div>
     </div>
   );
