c91e1edb379e609c2eefe903e66702339ae5da37
Giuseppe Senettone <92723630+giusene@users.noreply.github.com>
2022-02-22 12:51:17 +0100

    Update index.jsx

--- src/components/CitiesFilter/index.jsx
+++ src/components/CitiesFilter/index.jsx
@@ -1,15 +1,11 @@
-
 // import { useEffect } from "react";
 import { useState } from "react";
 // import { cities } from "./../../libs/cities";
-// import { useEffect, useState } from "react";
-import { cities } from "./../../libs/cities";
 import { provData } from "./../../libs/prov";
 import styles from "./CitiesFilter.module.scss";
 
 const CitiesFilter = ({ filter, setFilter, town, city }) => {
   const [prov, setProv] = useState(city);
-
   // const [changeTown, setChangeTown] = useState(town);
   // const [filteredCities, setFilteredCities] = useState([]);
 
@@ -27,7 +23,6 @@ const CitiesFilter = ({ filter, setFilter, town, city }) => {
     });
   };
 
-
   return (
     <>
       <select
@@ -43,7 +38,6 @@ const CitiesFilter = ({ filter, setFilter, town, city }) => {
         ))}
       </select>
 
-
       {/* <select
         className={styles.select}
         name="town"
@@ -52,14 +46,6 @@ const CitiesFilter = ({ filter, setFilter, town, city }) => {
         onChange={e => {
           setChangeTown(e.target.value);
           setFilter({ ...filter, town: e.target.value });
-      <select
-        className={styles.select}
-        name="town"
-        value={changeTown}
-        onChange={(e) => {
-        setChangeTown(e.target.value)
-        setFilter({...filter, town: e.target.value})
-
         }}
       >
         {filteredCities.map((city, index) => (
@@ -67,9 +53,7 @@ const CitiesFilter = ({ filter, setFilter, town, city }) => {
             {city.nome}
           </option>
         ))}
-
       </select> */}
-
     </>
   );
 };
